87f1f43acd8003b2f647f6a7656c3610ade64515
Manan Goel <manan.goel@research.iiit.ac.in>
2018-08-21 11:09:20 +0530

    PitFalls implemented

--- run.py
+++ run.py
@@ -2,29 +2,46 @@ import os
 
 from board import board
 from chars import mario
-from scenery import cloud
+from scenery import cloud, pit
 
 BOARD = board()
 MARIO = mario()
 begin = [0]
 cloud_list = []
+pit_list = []
 
 while True:
     os.system('tput reset')
     MARIO.draw(BOARD.canvas, begin)
-    #print(cloud_list)
+
     x = cloud()
-    #print(x.pos_x, x.pos_y)
+
     if cloud_list != []:
         for clouds in cloud_list:
-            if (x.pos_y - clouds.pos_y) > 10:
+            if abs(x.pos_y - clouds.pos_y) > 10:
                 if len(cloud_list) <= 20:
                     x.draw(BOARD.canvas, begin)
                     cloud_list.append(x)
     else:
         cloud_list.append(x)
         x.draw(BOARD.canvas, begin)
-        #print(cloud_list)
+    
+    y = pit()
+
+    if pit_list != []:
+        for p in pit_list:
+            if abs(p.start - y.start) > 10:
+                if len(pit_list) <= 10:
+                    y.draw(BOARD.canvas)
+                    pit_list.append(y)
+    else:
+        pit_list.append(y)
+        y.draw(BOARD.canvas)
+    
+    for p in pit_list:
+        if p.check_fall(MARIO, begin) is False:
+            quit()
+    
 
     BOARD.draw(begin)
     MARIO.move_mario(BOARD, BOARD.canvas, begin)
--- scenery.py
+++ scenery.py
@@ -1,5 +1,5 @@
 import random
-from globals import *
+from globals import NUM_STONE_ROWS
 
 class cloud():
 
@@ -20,4 +20,21 @@ class cloud():
     
     def __str__(self):
         return str(self.pos_x) + ',' + str(self.pos_y)
+
+class pit():
+    
+    def __init__(self):
+        self.start = random.randint(20, NUM_COLS-20)
+        self.end = random.randint(self.start, self.start + 7)
+    
+    def draw(self, canvas):
+        for i in range(self.start, self.end):
+            for j in range(NUM_STONE_ROWS[0], NUM_STONE_ROWS[1]):
+                canvas[j][i] = ' '
+
+    def check_fall(self, mario, begin):
+        if mario.pos[1] + begin[0] > self.start and mario.pos[1] + begin[0] < self.end:
+            return False
+        else:
+            return True        
                 
